79fbda1f76aea988b29c3c627bf81efedc57d396
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-20 22:11:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,53 +2,50 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
     set :views, 'app/views'
-  end
-
-  get '/' do
-
-  end
-
-  get '/posts/new' do
-    erb :new
-  end
-
-  post '/posts' do
-    Post.create(params)
-    redirect '/posts'
-  end
-
-  get '/posts' do
-    @posts = Post.all
-    erb :index
-  end
-
-  get '/posts/:id' do
-    @post = Post.find(params["id"])
-    erb :show
-  end
-
-  get '/posts/:id/edit' do
-    @post = Post.find(params["id"])
-    erb :edit
-  end
-
-  patch '/posts/:id' do
-    id = params["id"]
-    new_params = {}
-    old_post = Post.find(id)
-    new_params[:name] = params["name"]
-    new_params[:content] = params["content"]
-    old_post.update(new_params)
-
-    redirect "/posts/#{id}"
-  end
-
-  delete '/posts/:id/delete' do
-    @post = Post.find(params["id"])
-    @post.destroy
-    erb :delete
-  end
-end
+    end	 
+  
+  get '/articles' do
+      @articles = Article.all
+      erb :index
+    end	  
+  
+    get '/articles/new' do
+      erb :new
+    end
+  
+    post '/articles' do
+      attrs = params
+      @article = Article.create(attrs)
+  
+      redirect to "/articles/#{@article.id}"
+    end
+  
+    get '/articles/:id' do
+      id = params[:id]
+      @article = Article.find_by(id: id)
+  
+      erb :show
+    end
+  
+    get '/articles/:id/edit' do
+      id = params[:id]
+      @article = Article.find_by(id: id)
+      erb :edit
+    end
+  
+    patch '/articles/:id' do
+      id = params[:id]
+      @article = Article.find_by(id: id)
+      attrs = params[:article]
+      @article.update(attrs)
+      redirect to "/articles/#{@article.id}"
+    end
+  
+      delete '/articles/:id' do
+      id = params[:id]
+      Article.destroy(id)
+      redirect to '/articles'
+      end
+  
+  end	
