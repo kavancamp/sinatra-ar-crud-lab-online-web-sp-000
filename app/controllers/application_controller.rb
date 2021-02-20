29e9c155f5205b0a4ce244d6876edcd612df118f
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-20 22:11:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,37 +3,37 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
 
     set :views, 'app/views'
-    end	 
-  
+    end
+
   get '/articles' do
       @articles = Article.all
       erb :index
-    end	  
-  
+    end
+
     get '/articles/new' do
       erb :new
     end
-  
+
     post '/articles' do
       attrs = params
       @article = Article.create(attrs)
-  
+
       redirect to "/articles/#{@article.id}"
     end
-  
+
     get '/articles/:id' do
       id = params[:id]
       @article = Article.find_by(id: id)
-  
+
       erb :show
     end
-  
+
     get '/articles/:id/edit' do
       id = params[:id]
       @article = Article.find_by(id: id)
       erb :edit
     end
-  
+
     patch '/articles/:id' do
       id = params[:id]
       @article = Article.find_by(id: id)
@@ -41,11 +41,11 @@ class ApplicationController < Sinatra::Base
       @article.update(attrs)
       redirect to "/articles/#{@article.id}"
     end
-  
+
       delete '/articles/:id' do
       id = params[:id]
       Article.destroy(id)
       redirect to '/articles'
       end
-  
+
   end	
