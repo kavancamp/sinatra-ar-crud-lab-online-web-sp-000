59c069d6a6e1bab6fe39475d22a84111035f18d0
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-20 22:18:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,45 +5,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     end
 
-  get '/articles' do
-      @articles = Article.all
-      erb :index
-    end
-
-    get '/articles/new' do
-      erb :new
-    end
-
-    post '/articles' do
-      attrs = params
-      @article = Article.create(attrs)
-
-      redirect to "/articles/#{@article.id}"
-    end
-
-    get '/articles/:id' do
-      id = params[:id]
-      @article = Article.find_by(id: id)
-
-      erb :show
-    end
-
-    get '/articles/:id/edit' do
-      id = params[:id]
-      @article = Article.find_by(id: id)
-      erb :edit
-    end
-
-    patch '/articles/:id' do
-      id = params[:id]
-      @article = Article.find_by(id: id)
-      attrs = params[:article]
-      @article.update(attrs)
-      redirect to "/articles/#{@article.id}"
-    end
-
-      delete '/articles/:id' do
-      id = params[:id]
-      Article.destroy(id)
-      redirect to '/articles'
+    get '/' do	
+        redirect to '/articles'
       end
+  
+      get '/articles' do
+        @articles = Article.all
+        erb :index
+      end
+  
+      get '/articles/new' do
+        @article = Article.new
+        erb :new
+      end
+  
+      post '/articles' do
+        @article = Article.create(title: params[:title], content: params[:content])
+        redirect to "/articles/#{@article.id}"
+      end
+  
+      get '/articles/:id' do
+        @article = Article.find(params[:id])
+        erb :show
+      end
+  
+      get '/articles/:id/edit' do
+        @article = Article.find(params[:id])
+        erb :edit
+      end
+  
+  
+      patch "/articles/:id" do
+        @article = Article.find(params[:id])
+        # @article.update(title: params[:title], content: params[:content])
+        @article.update(params[:article])
+        redirect to "/articles/#{ @article.id }"
+      end
+  
+  
+      delete '/articles/:id' do
+        Article.delete(params[:id])
+        redirect to "/articles"
+      end
+    end	 
